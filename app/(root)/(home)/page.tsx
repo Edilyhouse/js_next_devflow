81d1e343020a7bed32398273022e59baebd4f298
Edy Lopez <edilyrouse@gmail.com>
2023-12-27 16:11:55 -0600

    Adding mobile Nav and Theme provider function

--- app/(root)/(home)/page.tsx
+++ app/(root)/(home)/page.tsx
@@ -1,9 +1,7 @@
-import { UserButton } from "@clerk/nextjs";
-
 export default function Home() {
   return (
     <div className="h-screen">
-      <UserButton afterSignOutUrl="/" />
+      <h1>cote feyo desde home</h1>
     </div>
   );
 }
