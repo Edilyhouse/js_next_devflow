815d41405cfcd20b4e1853bd3345ef93f240ab8f
Edy Lopez <edilyrouse@gmail.com>
2023-12-26 18:21:53 -0600

    adding routes and auth-clerk

--- app/page.tsx
+++
@@ -1,9 +0,0 @@
-export default function Home() {
-  return (
-    <div>
-      <h1 className="h1-bold">Next we are comming cote feyo</h1>
-      <h2 className="h2-bold">Next we are comming cote feyo</h2>
-      <h3 className="h3-bold">Next we are comming cote feyo</h3>
-    </div>
-  );
-}
