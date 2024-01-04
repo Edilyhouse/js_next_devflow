d33f208b05f14a451c71a7ec58c71a87a5798b2b
Edy Lopez <edilyrouse@gmail.com>
2024-01-04 14:44:09 -0600

    adding api webhook to public routes

--- app/api/webhook/route.ts
+++ app/api/webhook/route.ts
@@ -8,7 +8,7 @@ import { NextResponse } from "next/server";
 
 export async function POST(req: Request) {
   // You can find this in the Clerk Dashboard -> Webhooks -> choose the webhook
-  // TODO:  add  your secret webhook to .env file
+
   const WEBHOOK_SECRET = process.env.WEBHOOK_SECRET;
 
   if (!WEBHOOK_SECRET) {
