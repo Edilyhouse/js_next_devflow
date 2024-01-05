0624bb5ecad05a9065ed644d009841a343f1e441
Edy Lopez <edilyrouse@gmail.com>
2024-01-04 21:00:41 -0600

    adding more routes as public

--- middleware.ts
+++ middleware.ts
@@ -3,15 +3,17 @@ import { authMiddleware } from "@clerk/nextjs";
 export default authMiddleware({
   publicRoutes: [
     "/",
+    "/ask-question",
     "/api/webhook",
     "question/:id",
+    "questions/:id",
     "/tags",
     "/tags/:id",
     "/profile/:id",
     "/community",
     "/jobs",
   ],
-  ignoredRoutes: ["/api/webhook", "/api/chatgpt"],
+  ignoredRoutes: ["/api/webhook", "/api/chatgpt", "/questions/:id"],
 });
 
 export const config = {
